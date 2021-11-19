b7e079147bb05b96c359c4286e1e32236662a296
Thajib Parakkal <thajib@gmail.com>
2021-11-18 23:17:32 -0600

    soem changes maded to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Fib Calculator</h1>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
